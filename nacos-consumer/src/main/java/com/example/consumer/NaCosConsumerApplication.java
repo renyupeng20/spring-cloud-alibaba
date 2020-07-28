dfae615c6ce0119b19d17700717452d27157faec
renyupeng <renyupeng20>
2020-07-28 15:04:59 +0800

    [--]

--- nacos-consumer/src/main/java/com/example/consumer/NaCosConsumerApplication.java
+++ nacos-consumer/src/main/java/com/example/consumer/NaCosConsumerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class NaCosConsumerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NaCosConsumerApplication.class, args);
     }
 
